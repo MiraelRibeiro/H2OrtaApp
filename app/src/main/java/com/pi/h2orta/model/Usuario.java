531de01881453da516eb106c635ceb19e4cf5913
Mirael Ribeiro <60005700+MiraelRibeiro@users.noreply.github.com>
2021-06-06 14:17:30 -0300

    atualizacao 05-06
    insersão tela  dados do pedido

---
+++ app/src/main/java/com/pi/h2orta/model/Usuario.java
@@ -0,0 +1,130 @@
+package com.pi.h2orta.model;
+
+import android.renderscript.ScriptIntrinsicYuvToRGB;
+
+public class Usuario {
+    private String id;
+    private String nome;
+    private String sobrenome;
+    private String user;
+    private String cpf;
+    private String email;
+    private String senha;
+    private String confSenha;
+    private String endereco;
+    private String cidade;
+    private String cep;
+    private String estado;
+    private String pais;
+
+
+    public void salvarDados() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getConfSenha() {
+        return confSenha;
+    }
+
+    public void setConfSenha(String confSenha) {
+        this.confSenha = confSenha;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+
+}
